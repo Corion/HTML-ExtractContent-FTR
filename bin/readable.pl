7d0de7ccf03bed29805ba552ae5b72d8be267c24
Max Maischein <corion@corion.net>
2016-01-30 13:29:04 +0100

    Add optional output as HTML

--- bin/readable.pl
+++ bin/readable.pl
@@ -2,6 +2,11 @@
 use strict;
 use LWP::Simple;
 use HTML::ExtractContent::FTR;
+use Getopt::Long;
+
+GetOptions(
+    'html' => \my $output_html,
+);
 
 my( $url ) = @ARGV;
 my $html = get $url;
@@ -12,6 +17,31 @@ my $extractor = HTML::ExtractContent::FTR->new(
     rules_folder => 'rules/',
 );
 my $info = $extractor->extract( $html, url => $url );
-print $info->title, "\n";
-print join " - ", $info->date, $info->author, "\n";
-print $info->body, "\n";
+
+sub get_html {
+    my( $el ) = @_;
+    if( $el ) {
+        return join "", map { $_->as_HTML } $el->content_list
+    } else {
+        return ''
+    }
+}
+
+if( $output_html ) {
+    print <<HTML;
+<html>
+<head><title>@{[ get_html( $info->title_tree ) ]}</title>
+<body>
+<h1>@{[ get_html( $info->title_tree ) ]}</h1>
+<small>
+@{[ get_html( $info->date_tree ) ]} - @{[ get_html( $info->author_tree ) ]}
+</small>
+@{[ get_html( $info->body_tree ) ]}
+</body>
+</html>
+HTML
+} else {
+    print $info->title, "\n";
+    print join " - ", $info->date, $info->author, "\n";
+    print $info->body, "\n";
+}
--- lib/HTML/ExtractContent/Pluggable.pm
+++ lib/HTML/ExtractContent/Pluggable.pm
@@ -80,9 +80,6 @@ sub new {
     bless $info => $class
 }
 
-sub tree_as_html {
-}
-
 sub tree_as_text {
     my($self, $attr) = @_;
     if( my $tree = $self->{$attr}) {
@@ -97,5 +94,10 @@ sub body { $_[0]->tree_as_text('body') }
 sub author { $_[0]->tree_as_text('author') }
 sub date { $_[0]->tree_as_text('date') }
 sub as_text { $_[0]->body }
+
+sub title_tree { $_[0]->{title} }
+sub body_tree { $_[0]->{body} }
+sub author_tree { $_[0]->{author} }
+sub date_tree { $_[0]->{date} }
 
 1;
